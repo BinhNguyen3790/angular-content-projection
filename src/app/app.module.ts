1b6cb60d07a45dc44ec27af5bd250f5aab07d17c
llckumadevelop <kumauser01@kuma.ink>
2024-04-03 09:49:45 +0700

    up zippy ngproject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ZippyBasicComponent } from './zippy-basic/zippy-basic.component';
 import { ZippyMultislotComponent } from './zippy-multislot/zippy-multislot.component';
+import { ZippyNgprojectComponent } from './zippy-ngproject/zippy-ngproject.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ZippyBasicComponent,
     ZippyMultislotComponent,
+    ZippyNgprojectComponent,
   ],
   imports: [
     BrowserModule,
